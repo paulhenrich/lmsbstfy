5676ae98d093d61b43e2987cdd658915d4bc511f
Paul Henrich <paul@henrich.me>
2015-02-26 12:01:45 -0500

    lein new heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/lmsbstfy-standalone.jar clojure.main -m lmsbstfy.web
---
+++ test/lmsbstfy/web_test.clj
@@ -0,0 +1,6 @@
+(ns lmsbstfy.web-test
+  (:require [clojure.test :refer :all]
+            [lmsbstfy.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
